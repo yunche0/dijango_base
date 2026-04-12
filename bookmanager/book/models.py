eaaae819cea76101b6fce6f6d855f4847ed6296d
cb <1620059368qq.com>
2026-04-12 10:42:03 +0800

    定义视图函数

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Book(models.Model):
+class BookInfo(models.Model):
     #id
     name=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
 
 class PeopleInfo(models.Model):
     name=models.CharField(max_length=10)
