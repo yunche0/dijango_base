07faeeb6726a66e340e27d8063aa082db9a28e3e
cb <1620059368qq.com>
2026-04-13 11:03:41 +0800

    静态资源的访问

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
+STATICFILES_DIRS=[
+        BASE_DIR / 'static',
+        ]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
