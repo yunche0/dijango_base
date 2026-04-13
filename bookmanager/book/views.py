2358a09d41dd4a27cb7f0af31a46d34dfdbef615
cb <1620059368qq.com>
2026-04-13 10:47:41 +0800

    base-dir

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -9,4 +9,8 @@ from django.http import HttpResponse
 def index(request):
 
 
-    return HttpResponse('ok')
+    #return HttpResponse('ok')
+    context={
+            'name':'点击有惊喜'
+    }
+    return render(request,'book/index.html',context=context)
